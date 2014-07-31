ff62abf45f4494344bbf45cabfc768856a97b359
tbuyus <tbuyus@tbuyus-ME05.(none)>
2014-07-31 19:30:46 +0100

    First commit for eclectic primer with all images 'remastered'

---
+++ pg14642/makeAugmented.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+echo "zipping into an epub"
+7za a -tzip EclecticPrimer-Augmented.epub @filestopack.txt
+echo "creating azw3 file for kindle"
+ebook-convert EclecticPrimer-Augmented.epub EclecticPrimer-Augmented.azw3
+echo "creating mobi file"
+ebook-convert EclecticPrimer-Augmented.epub EclecticPrimer-Augmented.mobi
+
